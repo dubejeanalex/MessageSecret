89580fc9bad96cc56da58102d9c7eee8b5e4657f
SLAY666 <Shany.Carle@gmail.com>
2019-02-28 16:37:58 -0500

    Caractere supplémentaire

--- secretMessage.py
+++ secretMessage.py
@@ -5,10 +5,13 @@ key=  3
 newMessage=""
 message = input("Entrez un message: ")
 for character in message:
-	position = alphabet.find(character)
-	newPosition = (position + key) % 26
-	newCharacter = alphabet[newPosition]
-	newMessage+=newCharacter
+	if character in alphabet:
+		position = alphabet.find(character)
+		newPosition = (position + key) % 26
+		newCharacter = alphabet[newPosition]
+		newMessage+=newCharacter
+	else: 
+		newMessage+=character
 
 print (newMessage)
 cleDechiffrage =  key*-1
