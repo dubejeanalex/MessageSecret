f6fe5fad7e0d4a58e6f6bdc2aa0549e204d55fcd
SLAY666 <Shany.Carle@gmail.com>
2019-02-28 16:30:12 -0500

    Crypter des messages entiers

--- secretMessage.py
+++ secretMessage.py
@@ -1,21 +1,20 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-print( "Veuillez choisir votre clé")
-key=  int (input())
 
-character = input("Entrez un caractère: ")
-position = alphabet.find(character)
+key=  3
+newMessage=""
+message = input("Entrez un message: ")
+for character in message:
+	position = alphabet.find(character)
+	newPosition = (position + key) % 26
+	newCharacter = alphabet[newPosition]
+	newMessage+=newCharacter
 
-print(position)
-newPosition = (position + key) % 26
-
-newCharacter = alphabet[newPosition]
-print ("The new character is: ",newCharacter)
-
+print (newMessage)
 cleDechiffrage =  key*-1
 nouvellePosition = (newPosition +cleDechiffrage) % 26
 
-print (alphabet [nouvellePosition])
+
 
 
 
